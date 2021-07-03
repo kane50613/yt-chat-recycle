986aff32f5e694187b48bc6faf6a314993990811
凱恩Kane <50613gary@gmail.com>
2021-07-03 14:22:49 +0800

    working on storage sync

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const prefix = '[Youtube Chat Recycle]'
 
 document.querySelector('iframe.style-scope.ytd-live-chat-frame')
 ?.addEventListener('load', async () => {
-
+	if(!await get('yt_chat_recycle_enabled', true))
+		return
 	if(!getChat())
 		return console.log(`${prefix} no Youtube chat found`)
 
 	console.log(`${prefix} start recycling!`)
 
-	let max = 5, count = 0
 
-	let m = new MutationObserver(e => {
-		count = getChat()?.children?.length
+	let m = new MutationObserver(async e => {
+		let max = await get('yt_chat_recycle_max', 20),
+			count = getChat()?.children?.length
 		if(e[0]?.addedNodes?.length > 0 && count > max)
 			for(let i = 0; i < count - max; i++)
 				getChat()?.children[0]?.remove()
@@ -21,6 +22,23 @@ document.querySelector('iframe.style-scope.ytd-live-chat-frame')
 		childList: true
 	})
 })
+
+async function get(v, d) {
+	return new Promise(res => {
+		chrome.storage.sync.get(v, r => {
+			r = r?.[v]
+			if(r === undefined || r === null)
+				chrome.storage.sync.set(makeObj(v, d))
+			res(r ?? d)
+		})
+	})
+}
+
+function makeObj(k, v) {
+	let o = {}
+	o[k] = v
+	return o
+}
 
 const getChat = () =>
 	document.querySelector('iframe.style-scope.ytd-live-chat-frame')?.contentDocument
--- popup/index.js
+++ popup/index.js
@@ -3,6 +3,25 @@
 	document.getElementById('enabled').checked = await get('yt_chat_recycle_enabled', true)
 	document.getElementById('max').value = max
 	document.getElementById('max_count').innerText = max.toString()
+
+	document.getElementById('enabled').addEventListener("change", () => {
+		chrome.storage.sync.set({
+			yt_chat_recycle_enabled: document.getElementById('enabled').value ?? true
+		})
+	})
+
+	document.getElementById('max').addEventListener('input', () => {
+		max = document.getElementById('max').value
+		document.getElementById('max_count').innerText = max
+	})
+
+	document.getElementById('max').addEventListener('change', () => {
+		max = document.getElementById('max').value
+		console.log(max)
+		chrome.storage.sync.set({
+			yt_chat_recycle_max: max ?? 20
+		})
+	})
 })()
 
 async function get(v, d) {
