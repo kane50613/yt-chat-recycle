d8c1a09f98493a8da0d7f341b1c30360bb75006d
凱恩Kane <50613gary@gmail.com>
2021-07-03 16:26:41 +0800

    fix switch page bug

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
 const prefix = '[Youtube Chat Recycle]'
 
-document.querySelector('iframe.style-scope.ytd-live-chat-frame')
-?.addEventListener('load', async () => {
+let m
+
+setTimeout(init, 3000)
+
+async function init() {
 	if(!await get('yt_chat_recycle_enabled', true))
-		return
-	if(!getChat())
-		return console.log(`${prefix} no Youtube chat found`)
+		return console.log(`${prefix} extension disabled :(`)
+
+	if(!getFrame()) {
+		console.log(`${prefix} frame not found`)
+		await sleep(5000)
+		return init()
+	}
+	if(!getChat()) {
+		console.log(`${prefix} no Youtube chat found`)
+		await sleep(5000)
+		return init()
+	}
+
+	let inv = setInterval(() => {
+		if(!getChat()) {
+			console.log(`${prefix} chat not found`)
+			init()
+			clearInterval(inv)
+		}
+	}, 1000)
 
 	console.log(`${prefix} start recycling!`)
 
+	await update()
 
-	let m = new MutationObserver(async e => {
-		let max = await get('yt_chat_recycle_max', 20),
-			count = getChat()?.children?.length
-		if(e[0]?.addedNodes?.length > 0 && count > max)
-			for(let i = 0; i < count - max; i++)
-				getChat()?.children[0]?.remove()
+	m = new MutationObserver(async e => {
+		if(
+			e[0]?.addedNodes?.length > 0 &&
+			e[0]?.addedNodes[0]?.className === 'style-scope yt-live-chat-item-list-renderer'
+		)
+			await update()
 	})
 
 	m.observe(getChat(), {
 		childList: true
 	})
-})
+
+	async function update() {
+		let max = await get('yt_chat_recycle_max', 20),
+			count = getChat()?.children?.length
+		if(count > max) {
+			for(let i = 0; i < count - max; i++)
+				getChat()?.children[0]?.remove()
+			console.log(`${prefix} recycled ${count - max} comment(s)!`)
+		}
+	}
+}
 
 async function get(v, d) {
 	return new Promise(res => {
@@ -40,6 +71,9 @@ function makeObj(k, v) {
 	return o
 }
 
+const sleep = async (ms) => await new Promise(r => setTimeout(r, ms))
+
+const getFrame = () => document.querySelector('iframe.style-scope.ytd-live-chat-frame')
+
 const getChat = () =>
-	document.querySelector('iframe.style-scope.ytd-live-chat-frame')?.contentDocument
-	?.querySelector('#items.style-scope.yt-live-chat-item-list-renderer')
+	getFrame()?.contentDocument?.querySelector('#items.style-scope.yt-live-chat-item-list-renderer')
